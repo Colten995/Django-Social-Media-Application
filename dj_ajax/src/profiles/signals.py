a90bcfcf7d8a694d1aed20e11fe2331f633ca3a3
colten995 <Cgoetz9919@conestogac.on.ca>
2025-03-24 20:41:00 -0400

    Created a post_save signal for when a user is created. Creates a new profile as well using the newly created user

---
+++ dj_ajax/src/profiles/signals.py
@@ -0,0 +1,16 @@
+from .models import Profile
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+# The sender is the user
+# The user that is created is the instance
+# Created is a boolean value indicating that an instance is created
+
+@receiver(post_save, sender=User)
+def post_save_create_profile(sender, instance, created, *args, **kwargs):
+    print(sender)
+    print(instance)
+    print(created)
+    if created:
+        Profile.objects.create(user=instance)
